03266c2b0584f4b0c57267fb441612251bd3943a
Shivnay <shivnayswamy83@gmail.com>
2019-08-26 21:09:32 +1200

    profile page navbar hamburger mobile view bug fixed

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 #FEATURE UPDATES
-
-DESIGN UPDATES
-Navigation bar 
-  animation
-  structure
-  Layout
-  Toggle Button
-
+**************************
+  TO DO
+**************************
+PROFILE SLIDE SHOW
+  Full screen profile cover toggle (clicked)
+    1. hide navbar and section navigation controls
+    2. Start profile summary animation
+    3. Exit anytime by selecting profile cover toggler
+  
 PERFORMANCE
 Loading Images (Node JS)
 Overall performance improvments
-  
+
--- assets/js/application.js
+++ assets/js/application.js
@@ -1,12 +1,6 @@
 var $input = $('<div class="modal-body"><input type="text" class="form-control" placeholder="Message"></div>')
 
 $(function(){
-  setTimeout(function(){
-    $(".peek-cover-mobile").css("left", "-1px");
-  }, 1000);
-});
-
-$(function(){
   var height = $(".profile-header").css("height");
   $(".peek-cover, .peek-cover-mobile").click(()=>{
     if ($(".profile-header").css("height") == height)
@@ -22,7 +16,7 @@ $(function () {
   $("#mood ul li").click(()=>{
     my_mood();
   })
-
+  //Function: bitmoji carisal animation
   function my_mood(){
     var mood = $("#mood ul");
     var mood_emojis = mood.children();
@@ -39,9 +33,7 @@ $(function () {
                 $("#mood ul .final").animate({
                     "fontSize" : "70px"
                 },500);
-            }
-            else
-            {
+            } else {
                 emoji_index++;
                 $("#mood ul li:nth-child("+emoji_index+") span img").css({"width":"120px", "height" : "120px"});
             }
@@ -96,26 +88,6 @@ $(function () {
 
 })
 
-$(document).on('click', '.js-gotoMsgs', function () {
-  $input.remove()
-  $('.js-conversation').addClass('hidden-xs-up')
-  $('.js-msgGroup, .js-newMsg').removeClass('hidden-xs-up')
-  $('.modal-title').html('Messages')
-})
-
-$(document).on('click', '[data-action=growl]', function (e) {
-  e.preventDefault()
-
-  $('#app-growl').append(
-    '<div class="alert alert-dark alert-dismissible fade show" role="alert">'+
-      '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
-        '<span aria-hidden="true">×</span>'+
-      '</button>'+
-      'Click the x on the upper right to dismiss this little thing. Or click growl again to show more growls'+
-    '</div>'
-  )
-})
-
 $(document).on('focus', '[data-action="grow"]', function () {
   if ($(window).width() > 1000) {
     $(this).animate({
@@ -132,95 +104,3 @@ $(document).on('blur', '[data-action="grow"]', function () {
   }
 })
 
-// back to top button - docs
-// $(function () {
-//   if ($('.docs-top').length) {
-//     _backToTopButton()
-//     $(window).on('scroll', _backToTopButton)
-//     function _backToTopButton () {
-//       if ($(window).scrollTop() > $(window).height()) {
-//         $('.docs-top').fadeIn()
-//       } else {
-//         $('.docs-top').fadeOut()
-//       }
-//     }
-//   }
-// })
-
-// $(function () {
-//     // doc nav js
-//     var $toc = $('#markdown-toc')
-//     $('#markdown-toc li').addClass('nav-item')
-//     $('#markdown-toc li > a').addClass('nav-link')
-//     var $window = $(window)
-
-//     if ($toc[0]) {
-
-//       maybeActivateDocNavigation()
-//       $window.on('resize', maybeActivateDocNavigation)
-
-//       function maybeActivateDocNavigation () {
-//         if ($window.width() > 768) {
-//           activateDocNavigation()
-//         } else {
-//           deactivateDocNavigation()
-//         }
-//       }
-
-//       function deactivateDocNavigation() {
-//         $window.off('resize.theme.nav')
-//         $window.off('scroll.theme.nav')
-//         $toc.css({
-//           position: '',
-//           left: '',
-//           top: ''
-//         })
-//       }
-
-//       function activateDocNavigation() {
-
-//         var cache = {}
-
-//         function updateCache() {
-//           cache.containerTop   = $('.docs-content').offset().top - 40
-//           cache.containerRight = $('.docs-content').offset().left + $('.docs-content').width() + 45
-//           measure()
-//         }
-
-//         function measure() {
-//           var scrollTop = $window.scrollTop()
-//           var distance =  Math.max(scrollTop - cache.containerTop, 0)
-
-//           if (!distance) {
-//             $($toc.find('li a')[1]).addClass('active')
-//             return $toc.css({
-//               position: '',
-//               left: '',
-//               top: ''
-//             })
-//           }
-
-//           $toc.css({
-//             position: 'fixed',
-//             left: cache.containerRight,
-//             top: 40
-//           })
-//         }
-
-//         updateCache()
-
-//         $(window)
-//           .on('resize.theme.nav', updateCache)
-//           .on('scroll.theme.nav', measure)
-
-//         $('body').scrollspy({
-//           target: '#markdown-toc',
-//           children: 'li > a'
-//         })
-
-//         setTimeout(function () {
-//           $('body').scrollspy('refresh')
-//         }, 1000)
-//       }
-//     }
-// })
